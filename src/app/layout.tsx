15360eab634debc6c757d3698daff7e05f27d44d
finmook <apimooksarovath@gmail.com>
2025-10-18 14:12:37 +0700

    feat/add detailed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./background.css";
-import { zfPixel } from "./font";
+import { zfPixel,prompt } from "./font";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${zfPixel.variable}`}>
+    <html lang="en" className={`${zfPixel.variable} ${prompt.variable}`}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
