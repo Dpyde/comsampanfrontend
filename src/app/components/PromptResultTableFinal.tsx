79d53f5c489724591991e9af7704568474362bd6
finmook <apimooksarovath@gmail.com>
2025-12-14 18:53:50 +0700

    nif/feat:update prompt

--- src/app/components/PromptResultTableFinal.tsx
+++ src/app/components/PromptResultTableFinal.tsx
@@ -7,7 +7,7 @@ const promptTeams=5;
 export default function PromptResultTableFinal() {
   return (
   <div className="overflow-x-auto">
-    <div className="relative flex justify-center gap-32 p-10 pl-80">
+    <div className="relative flex justify-center gap-32 p-10 pl-40">
       <Xwrapper>
         {/* first */}
         <div className="flex flex-col justify-around gap-12">
@@ -21,34 +21,21 @@ export default function PromptResultTableFinal() {
         </div>
 
         {/* second */}
-        <div className="flex flex-col justify-center gap-64">
-          <div id="p2a">
-            <ResultTable matches={[
-              { team: "ทีม A", rank: 0, id:"p2a_a" }]}/>
-            <ResultTable matches={[
-              { team: "ทีม B", rank: 0, id:"p2a_b" }]}/>
-            <ResultTable matches={[
-              { team: "ทีม C", rank: 0, id:"p2a_c" }]}/>
-            <ResultTable matches={[
-              { team: "ทีม D", rank: 0, id:"p2a_d" }]}/>
-          </div>
-        </div>
+        
 
         {/* third */}
         <div className="flex flex-col justify-center">
             <ResultTable matches={[
-              { team: "ทีม A", rank: 0, id:"p3a",
+              { team: "เจมินิ", rank: 0, id:"p3a",
                 //  icon:"gold"
                  },
-              { team: "ทีม F", rank: 0, id:"p3b", 
-                // icon:"silver"
-               }
+              
             ]}/>
         </div>
 
         <Xarrow
           start="p1a"
-          end="p2a"
+          end="p3a"
           path="grid"      
           curveness={0}
           gridBreak="50%"
@@ -59,7 +46,7 @@ export default function PromptResultTableFinal() {
         />
         <Xarrow
           start="p1b"
-          end="p2a"
+          end="p3a"
           path="grid"
           curveness={0}
           gridBreak="50%"
@@ -70,7 +57,7 @@ export default function PromptResultTableFinal() {
         />
         <Xarrow
           start="p1c"
-          end="p2a"
+          end="p3a"
           path="grid"
           curveness={0}
           gridBreak="50%"
@@ -81,7 +68,7 @@ export default function PromptResultTableFinal() {
         />
         <Xarrow
           start="p1d"
-          end="p2a"
+          end="p3a"
           path="grid"
           curveness={0}
           gridBreak="50%"
@@ -92,7 +79,7 @@ export default function PromptResultTableFinal() {
         />
         <Xarrow
           start="p1e"
-          end="p2a"
+          end="p3a"
           path="grid"
           curveness={0}
           gridBreak="50%"
@@ -102,27 +89,8 @@ export default function PromptResultTableFinal() {
           endAnchor={{ position: "left", offset: { y: 0 } }}
         />
         
-        <Xarrow
-          start="p1e"
-          end="p3b"
-          path="grid"
-          curveness={0}
-          gridBreak="90%"
-          color="rgba(255, 255, 255, 1)"
-          strokeWidth={2}
-          showHead={false}
-          startAnchor={{ position: "right", offset: { y: 20 } }}
-        />
-        <Xarrow
-          start="p2a"
-          end="p3a"
-          path="grid"
-          curveness={0}
-          gridBreak="50%"
-          color="rgba(255, 255, 255, 1)"
-          strokeWidth={2}
-          showHead={false}
-        />
+        
+        
       </Xwrapper>
     </div>
   </div>
