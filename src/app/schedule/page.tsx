4311659adea01860dea664627b56c7630230a40b
finmook <apimooksarovath@gmail.com>
2025-12-14 12:09:53 +0700

    fixed:schedule padding

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -5,8 +5,8 @@ import NavBar from '../components/navbar';
 const page = () => {
   return (
     <div className='flex flex-col items-center w-full min-h-screen bg-darkblue'>
-        <NavBar />
-        <div>
+        {/* <NavBar /> */}
+        <div className="px-8">
              <div className='pixel-font text-[#B2F8E7] text-[64px] lg:text-[72px] mt-25'>Schedule</div>
             <img className='mt-12 w-full max-w-[1080px] h-auto' src="timeline/timeline.png" alt="" />
 
